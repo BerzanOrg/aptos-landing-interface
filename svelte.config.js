cfc9f0fb146633a23f3f0e12769d69cedae181d7
Berzan <berzan@tutanota.com>
2023-10-01 16:09:44 +0000

    update: config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -14,7 +16,7 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html',
 			paths: {
-				base: process.argv.includes('dev') ? '' : '/aptos-landing-interface'
+				base:  dev ? '' : '/aptos-landing-interface'
 			}
 		})
 	}
